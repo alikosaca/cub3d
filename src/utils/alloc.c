05e00c7f8fcf98d1377e4bf3a450f71151accfff
yemreaycicek <yunus.e.aycicek@gmail.com>
2025-09-08 18:30:08 +0300

    style: standardize pointer dereferencing in alloc.c

--- src/utils/alloc.c
+++ src/utils/alloc.c
@@ -6,7 +6,7 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 13:41:28 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/09/03 15:41:09 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/09/08 18:28:48 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*_alloc(void **ptr, size_t nmemb, size_t size)
 {
 	if (!ptr)
 		return (NULL);
-	*ptr = ft_calloc(nmemb, size);
+	(*ptr) = ft_calloc(nmemb, size);
 	if (!(*ptr))
 		return (NULL);
 	return ((*ptr));
@@ -26,7 +26,7 @@ void	*__alloc(void ***ptr, size_t nmemb, size_t size)
 {
 	if (!ptr)
 		return (NULL);
-	*ptr = ft_calloc(nmemb, size);
+	(*ptr) = ft_calloc(nmemb, size);
 	if (!(*ptr))
 		return (NULL);
 	return ((*ptr));
