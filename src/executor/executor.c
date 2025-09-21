bcf54da7b491b72a5b25696a87b56e33645e7f5e
alikosaca <alikosaca0@gmail.com>
2025-09-21 13:18:58 +0300

    fix: correct return statement in init_game function and update executor logic

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -3,19 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
+/*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 12:15:35 by akosaca           #+#    #+#             */
-/*   Updated: 2025/09/11 13:05:31 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/09/21 13:13:22 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static int	init_game(void **mlx)
+{
+	if (init_mlx(mlx))
+		returen (1);
+	return (0);
+}
+
 int	executor(t_exec *exec, t_pars *pars)
 {
 	(void)pars;
-	if (init_mlx(&exec->mlx))
+	if (init_game(&exec->mlx))
 		return (1);
 	mlx_destroy_display(exec->mlx);
 	_free(&exec->mlx);
