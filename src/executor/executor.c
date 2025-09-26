e145f3bdb7940c942f77dbb9689054442f621ed4
alikosaca <alikosaca0@gmail.com>
2025-09-26 15:05:16 +0300

    refactor: simplify function parameters and update texture path handling

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,32 +6,26 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 12:15:35 by akosaca           #+#    #+#             */
-/*   Updated: 2025/09/21 15:48:08 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/09/26 15:00:53 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-static int	init_game(t_exec **exec, t_pars *pars)
+#include <stdio.h>
+static int	init_game(t_exec *exec, t_pars *pars)
 {
-	int x_line;
-	int y_line;
 
-	x_line = pars->map.max_w;
-	y_line = pars->map.h;
-	(*exec)->tex_path = (t_tex_path){0};
-	if (init_mlx(&(*exec)->mlx))
+	exec->tex_path = (t_tex_path){0};
+	if (init_mlx(&(exec->mlx)))
 		return (1);
-	if (create_win(&(*exec)->mlx, &(*exec)->win, x_line, y_line))
-		return (1);
-	if (create_xpm((*exec)->mlx, &(*exec)->tex_path, &pars->tex))
+	if (create_xpm(exec->mlx, exec->tex_path, &pars->tex))
 		return (1);
 	return (0);
 }
-#include <stdio.h>
+
 int	executor(t_exec *exec, t_pars *pars)
 {
-	if (init_game(&exec, pars))
+	if (init_game(exec, pars))
 		return (1);
 	return (0);
 }
--- src/executor/map_init.c
+++ src/executor/map_init.c
@@ -6,7 +6,7 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 21:32:23 by akosaca           #+#    #+#             */
-/*   Updated: 2025/09/21 15:52:37 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/09/26 15:00:25 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,16 @@ int	init_mlx(void **mlx)
 	return (0);
 }
 
-int	create_win(void **mlx, void **win, int p_x, int p_y)
-{
-	int	x_px;
-	int	y_px;
 
-	x_px = p_x * 64;
-	y_px = p_y * 64;
-	(*win) = mlx_new_window((*mlx), x_px, y_px, "cub3d");
-	if (!(*win))
-		return (1);
-	return (0);
-}
-int	create_xpm(void *mlx, t_tex_path *path, const t_tex *tex)
+int	create_xpm(void *mlx, t_tex_path path, const t_tex *tex)
 {
 	int x;
 	int y;
-	path->ea = mlx_xpm_file_to_image(mlx, tex->ea, &x, &y);
-	path->no = mlx_xpm_file_to_image(mlx, tex->no, &x, &y);
-	path->so = mlx_xpm_file_to_image(mlx, tex->so, &x, &y);
-	path->we = mlx_xpm_file_to_image(mlx, tex->we, &x, &y);
-	if (!path->ea || !path->no || !path->so || !path->we)
+	path.ea = mlx_xpm_file_to_image(mlx, tex->ea, &x, &y);
+	path.no = mlx_xpm_file_to_image(mlx, tex->no, &x, &y);
+	path.so = mlx_xpm_file_to_image(mlx, tex->so, &x, &y);
+	path.we = mlx_xpm_file_to_image(mlx, tex->we, &x, &y);
+	if (!path.ea || !path.no || !path.so || !path.we)
 		return (1);
 	return(0);
 }
