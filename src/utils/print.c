9987e2894377e2e6c7c4c9c8814da3405e30205a
yemreaycicek <yunus.e.aycicek@gmail.com>
2025-10-01 17:24:13 +0300

    refactor: simplify error message formatting in print()

--- src/utils/print.c
+++ src/utils/print.c
@@ -6,24 +6,17 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 18:46:28 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/09/03 15:55:00 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/10/01 15:50:37 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
-#define BOLD   "\e[1m"
-#define RED    "\e[31m"
-#define RESET  "\e[0m"
-
-#define ERROR "Error"
+#define ERROR "\e[1m\e[31mError\e[0m"
 
 int	print(char *str)
 {
-	ft_putstr_fd(BOLD, STDERR_FILENO);
-	ft_putstr_fd(RED, STDERR_FILENO);
-	ft_putstr_fd(ERROR, STDERR_FILENO);
-	ft_putendl_fd(RESET, STDERR_FILENO);
+	ft_putendl_fd(ERROR, STDERR_FILENO);
 	if (str)
 		ft_putendl_fd(str, STDERR_FILENO);
 	return (1);
