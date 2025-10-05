81443c30c87053adce241613887fd2b62f59e141
yemreaycicek <yunus.e.aycicek@gmail.com>
2025-10-05 18:17:31 +0300

    fix: correct usage message in ERR_INVALID_ARGS definition

--- include/errors.h
+++ include/errors.h
@@ -6,14 +6,14 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 18:43:24 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/10/02 16:36:29 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/10/05 18:16:22 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_H
 # define ERRORS_H
 
-# define ERR_INVALID_ARGS "Usage: .cub3D <map.cub>"
+# define ERR_INVALID_ARGS "Usage: ./cub3D <map.cub>"
 # define ERR_INVALID_EXTENSION "Map file must have a .cub extension"
 # define ERR_FILE_MISS_OR_UNREAD "Map file does not exist or is not readable"
 # define ERR_EMPTY_MAP "Map file cannot be empty"
