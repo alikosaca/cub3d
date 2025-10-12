5598c77aadd5aa21b59005239e8c1e7926b6eee8
alikosaca <alikosaca0@gmail.com>
2025-10-12 19:18:22 +0300

    fix(memory): resolve double free issue and remove exit.c

--- src/executor/hooks.c
+++ src/executor/hooks.c
@@ -6,7 +6,7 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 01:42:28 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/10/10 17:13:49 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/10/12 15:53:40 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ static int	destroy_window(void *ptr)
 	exit(cleanup((t_game *)ptr));
 }
 
-static int	handle_key(int keycode, t_game *game)
+static int	handle_key(int keycode, void *ptr)
 {
 	if (keycode == ESC)
-		exit(cleanup(game));
+		exit(cleanup((t_game *)ptr));
 	return (0);
 }
 
 void	init_hooks(t_game *game, void *win)
 {
 	mlx_hook(win, 17, 0, destroy_window, game);
-	mlx_key_hook(win, handle_key, &game);
+	mlx_key_hook(win, handle_key, game);
 }
