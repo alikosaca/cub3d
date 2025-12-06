5bb81a288c7bc8a40a5073cfffc1c7329a2dbb26
yemreaycicek <yunus.e.aycicek@gmail.com>
2025-12-06 11:41:15 +0300

    fix: change usage error exit code from 1 to 2

--- src/verify.c
+++ src/verify.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   verify.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yaycicek <yaycicek@student.42istanbul.com. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 16:42:29 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/09/17 15:31:05 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/12/06 11:39:11 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,10 @@ static int	check_file_extension(char *file)
 void	verify(int argc, char *file)
 {
 	if (argc != 2)
-		exit(print(ERR_INVALID_ARGS));
+	{
+		print(ERR_INVALID_ARGS);
+		exit(2);
+	}
 	if (check_file_extension(file))
 		exit(print(ERR_INVALID_EXTENSION));
 	if (is_file_readable(file))
