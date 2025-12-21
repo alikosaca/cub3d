0b746f7838e2ad6195101aaf9cb776c00faeadc4
alikosaca <alikosaca0@gmail.com>
2025-12-21 11:08:26 +0300

    feat: add raycasting structures and player position to executor.h,,
    - Added raycasting structs to executor.h:
      * t_ray - Ray projection calculations
      * t_ply - Player position and direction
    - Implemented ray_loop function signature
    - Prepared architecture for 3D rendering pipeline

--- include/executor.h
+++ include/executor.h
@@ -6,21 +6,24 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 15:34:46 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/10/18 13:55:29 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/12/21 11:06:08 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTOR_H
 # define EXECUTOR_H
 
+# include <math.h>
 # include "mlx.h"
 # include "parser.h"
 
 # define PROJECT_NAME "cub3D"
 // # define SCREEN_WIDTH 3840
 // # define SCREEN_HEIGHT 2160
-# define SCREEN_WIDTH 1920
-# define SCREEN_HEIGHT 1000
+// # define SCREEN_WIDTH 1920
+// # define SCREEN_HEIGHT 1000
+# define SCREEN_WIDTH 190
+# define SCREEN_HEIGHT 100
 
 # define ESC 65307
 # define A 97
@@ -57,11 +60,45 @@ typedef struct s_img
 	int		height;
 }	t_img;
 
+typedef struct s_ray
+{
+	double	camera_x;
+	double	ray_dir_x;
+	double	ray_dir_y;
+	int		map_x;
+	int		map_y;
+	double	delta_dist_x;
+	double	delta_dist_y;
+	double	side_dist_x;
+	double	side_dist_y;
+	int		step_x;
+	int		step_y;
+	int		hit;
+	int		side;
+	double	perp_wall_dist;
+	int		line_height;
+	int		draw_start;
+	int		draw_end;
+}	t_ray;
+
+typedef struct s_player
+{
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+}	t_ply;
+
+
 typedef struct s_exec
 {
 	t_mlx	mlx;
 	t_xpm	xpm;
 	t_img	img;
+	t_ray	ray;
+	t_ply	ply;
 }	t_exec;
 
 int		executor(t_game *game, t_exec *exec, t_pars *pars);
@@ -69,16 +106,15 @@ int		executor(t_game *game, t_exec *exec, t_pars *pars);
 int		init_mlx(void **mlx);
 int		create_xpm(void *mlx, t_xpm *xpm, t_tex tex);
 int		create_win(void *mlx, void **win);
-int		read_map(void *mlx, void *win, t_map map);
+//int		read_map(void *mlx, void *win, t_map map);
 int		init_img_data(t_img *img_data, void *mlx);
 
 void	init_hooks(t_game *game, void *mlx);
 void	destroy_exec(t_exec *exec);
 
-//void	render_frame(t_exec *exec, t_pars *pars);
 void	draw_static_background(t_img *img);
 
+int		ray_loop(t_exec *exec);
 
-
-void put_window(void *mlx, void *win, int x, int y, int color);
+//void put_window(void *mlx, void *win, int x, int y, int color);
 #endif
