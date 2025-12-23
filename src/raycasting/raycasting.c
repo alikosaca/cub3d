3102b3fcb16e211eb38347b08a47c395c8e7df90
alikosaca <alikosaca0@gmail.com>
2025-12-23 23:56:16 +0300

    feat: implement DDA step and side distance calculation
    - Added init_step_and_side_dist() function:
      * Determines step direction based on ray angle (res_x, res_y)
      * Calculates initial side distances for DDA algorithm:
        - side_dist_x: distance to first vertical grid line
        - side_dist_y: distance to first horizontal grid line
      * Properly handles both positive and negative ray directions
    - Key algorithm logic:
      * step_x/step_y: Grid traversal direction (-1 left/down, +1 right/up)
      * side_dist: Distance from player to next grid boundary
      * Uses delta_dist as scaling factor for efficiency
    - Mathematical foundations:
      * Conditional calculations based on ray_dir sign
      * Accounts for player's fractional position within cell
      * Prepares for efficient DDA wall collision detection

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -6,12 +6,35 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 15:56:33 by akosaca           #+#    #+#             */
-/*   Updated: 2025/12/21 11:10:07 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/12/23 23:55:01 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include <stdio.h>
+
+static void	init_step_and_side_dist(t_ray *ray, t_ply *ply)
+{
+	int	res_x;
+	int	res_y;
+
+	res_x = 1;
+	res_y = 1;
+	if (ray->ray_dir_x < 0)
+		res_x *= -1;
+	if (ray->ray_dir_y < 0)
+		res_y *= -1;
+	if (ray->ray_dir_x < 0)
+		ray->side_dist_x = (ply->pos_x - ray->map_x) * ray->delta_dist_x;
+	else
+		ray->side_dist_x = (ray->map_x + 1.0 - ply->pos_x) * ray->delta_dist_x;
+	if (ray->ray_dir_y < 0)
+		ray->side_dist_y = (ply->pos_y - ray->map_y) * ray->delta_dist_y;
+	else
+		ray->side_dist_y = (ray->map_y + 1.0 - ply->pos_y) * ray->delta_dist_y;
+	ray->step_x = res_x;
+	ray->step_y = res_y;
+}
 
 static void	init_ray(t_ray *ray, t_ply *ply, int x)
 {
@@ -29,6 +52,8 @@ static void	init_ray(t_ray *ray, t_ply *ply, int x)
 		ray->delta_dist_y = 1e30;
 	else
 		ray->delta_dist_y = fabs(1 / ray->ray_dir_y);
+	init_step_and_side_dist(ray, ply);
+	
 }
 
 int	ray_loop(t_exec *exec)
