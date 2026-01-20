8730b72a0bfe121d637d0493c8849d1d5f1829ce
yemreaycicek <yunus.e.aycicek@gmail.com>
2026-01-20 19:33:32 +0300

    fix: remove verify_map function and its usage in validate_map

--- src/parser/map_validate.c
+++ src/parser/map_validate.c
@@ -12,32 +12,10 @@
 
 #include "parser.h"
 
-int	verify_map(char *map[])
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (map[i])
-	{
-		j = 0;
-		while (map[i][j])
-		{
-			if (!ft_strchr("10NSEW ", map[i][j]))
-				return (print(ERR_INVALID_MAP_CHAR));
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
-
 int	validate_map(t_map *map)
 {
 	char	**copy;
 
-	if (verify_map(map->map))
-		return (1);
 	if (alloc_copy(&copy, map->map, map->h, map->max_w))
 		return (1);
 	if (flood_fill(map, copy))
